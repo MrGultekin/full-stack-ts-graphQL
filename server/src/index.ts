2705cd66663b8c41d6a83fb88a01c10a26c55723
MrGultekin <gultekin.de@gmail.com>
2022-08-30 22:34:40 +0200

    apolloServer and httpServer with their imports added.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,8 @@ import { watchClientBuild } from './build-client';
 import { DB_FILE_PATH, PORT, STATIC_ROOT_FOLDER_PATH } from './constants';
 import Db from './db';
 import { seedDb } from './seed';
+import { createApolloServer } from './apollo-server';
+import { createServer } from 'http';
 
 const app = express();
 
@@ -20,6 +22,12 @@ async function main() {
   await seedDb(db);
 
   app.use('/static', express.static(STATIC_ROOT_FOLDER_PATH));
+  const httpServer = createServer(app)
+  const apolloServer = await createApolloServer(
+    db,
+    httpServer,
+    app
+  )
 
   await new Promise<void>((resolve) =>
     app.listen(PORT, () => {
